f1ea585ff4f15fea91af1fb061ca300624c5f84c
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-03-04 13:39:08 +0900

    Initial Commit

---
+++ ClearStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClearStyle
+//
+//  Created by Jaewon Choi on 2015. 3. 4..
+//  Copyright (c) 2015년 Jaewon Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClearStyle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClearStyle
+//
+//  Created by Jaewon Choi on 2015. 3. 4..
+//  Copyright (c) 2015년 Jaewon Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
